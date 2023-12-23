a2d7f1c65e81d8db2f486a26f23f2b7499702ad0
Daniil Kononenko <rise.di.o3@gmail.com>
2023-12-23 17:51:18 +0300

    First commit

---
+++ lexicon/lexicon.py
@@ -0,0 +1,9 @@
+LEXICON: dict[str, str] = {
+    '/start': 'Привет, бот запущен. Информацию смотри по команде <b>/help<b>',
+
+    '/help': '/start - запуск бота\n/help - справка',
+}
+
+LEXICON_COMMANDS: dict[str, str] = {
+    '/help': 'Справка по работе бота'
+}
